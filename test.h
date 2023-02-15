c75e226a9a87e7718b6c99b39d3d26e108e7105b
chengweisheng <84381233+chengweisheng@users.noreply.github.com>
2023-02-15 11:00:54 +0800

    Create test.h

---
+++ test.h
@@ -0,0 +1,64 @@
+#ifndef      _COMMON_H//和文件名一致，将.换成_；前面加_。
+	#define     _COMMON_H//避免重复声明，使用if no define,define......endif
+	#include<stdio.h>//添加程序所需要的所有头文件
+	#include<string.h>
+	static int a = 0 ；//可以定义静态变量，在所有.c文件中使用。
+	int  test(char* p,int n)//声明所有子函数
+	void  mycopy(char* a,int i,int j)
+	#endif
+	#ifndef      _COMMON_H//和文件名一致，将.换成_；前面加_。
+	#define     _COMMON_H//避免重复声明，使用if no define,define......endif
+	#include<stdio.h>//添加程序所需要的所有头文件
+	#include<string.h>
+	static int a = 0 ；//可以定义静态变量，在所有.c文件中使用。
+	int  test(char* p,int n)//声明所有子函数
+	void  mycopy(char* a,int i,int j)
+	#endif
+	#ifndef      _COMMON_H//和文件名一致，将.换成_；前面加_。
+	#define     _COMMON_H//避免重复声明，使用if no define,define......endif
+	#include<stdio.h>//添加程序所需要的所有头文件
+	#include<string.h>
+	static int a = 0 ；//可以定义静态变量，在所有.c文件中使用。
+	int  test(char* p,int n)//声明所有子函数
+	void  mycopy(char* a,int i,int j)
+	#endif
+	#ifndef      _COMMON_H//和文件名一致，将.换成_；前面加_。
+	#define     _COMMON_H//避免重复声明，使用if no define,define......endif
+	#include<stdio.h>//添加程序所需要的所有头文件
+	#include<string.h>
+	static int a = 0 ；//可以定义静态变量，在所有.c文件中使用。
+	int  test(char* p,int n)//声明所有子函数
+	void  mycopy(char* a,int i,int j)
+	#endif
+  #ifndef      _COMMON_H//和文件名一致，将.换成_；前面加_。
+	#define     _COMMON_H//避免重复声明，使用if no define,define......endif
+	#include<stdio.h>//添加程序所需要的所有头文件
+	#include<string.h>
+	static int a = 0 ；//可以定义静态变量，在所有.c文件中使用。
+	int  test(char* p,int n)//声明所有子函数
+	void  mycopy(char* a,int i,int j)
+	#endif
+	#ifndef      _COMMON_H//和文件名一致，将.换成_；前面加_。
+	#define     _COMMON_H//避免重复声明，使用if no define,define......endif
+	#include<stdio.h>//添加程序所需要的所有头文件
+	#include<string.h>
+	static int a = 0 ；//可以定义静态变量，在所有.c文件中使用。
+	int  test(char* p,int n)//声明所有子函数
+	void  mycopy(char* a,int i,int j)
+	#endif
+	#ifndef      _COMMON_H//和文件名一致，将.换成_；前面加_。
+	#define     _COMMON_H//避免重复声明，使用if no define,define......endif
+	#include<stdio.h>//添加程序所需要的所有头文件
+	#include<string.h>
+	static int a = 0 ；//可以定义静态变量，在所有.c文件中使用。
+	int  test(char* p,int n)//声明所有子函数
+	void  mycopy(char* a,int i,int j)
+	#endif
+	#ifndef      _COMMON_H//和文件名一致，将.换成_；前面加_。
+	#define     _COMMON_H//避免重复声明，使用if no define,define......endif
+	#include<stdio.h>//添加程序所需要的所有头文件
+	#include<string.h>
+	static int a = 0 ；//可以定义静态变量，在所有.c文件中使用。
+	int  test(char* p,int n)//声明所有子函数
+	void  mycopy(char* a,int i,int j)
+	#endif
